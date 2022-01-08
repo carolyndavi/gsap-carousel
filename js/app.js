f61e1639d45f4bcfcecc6c19907a7e077a304bd2
Carolyn Ndavi <cn.ndavi@hotmail.com>
2022-01-08 12:34:35 +0100

    gsap update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const carouselContainer = document.querySelector('.carousel-track-container');
+// selecting elements
+const carouselContainer = document.querySelector('.carousel-container');
 const carouselTrack = document.querySelector('.carousel-track');
 const carouselSlides = Array.from(document.querySelectorAll('.carousel-slide'));
 const carouselNavigation = document.querySelector('.carousel-navigation');
@@ -8,19 +9,18 @@ const carouselIndicator = Array.from(
   document.querySelectorAll('.carousel-indicator')
 );
 
-console.log(carouselSlides, carouselIndicator);
-
 const carouselImages = [
   'https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
   'https://images.unsplash.com/photo-1545394734-b4140a8cfa9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
   'https://images.unsplash.com/photo-1595131264251-63b7cf3b8564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
 ];
 
-const slideWidth = carouselSlides[0].getBoundingClientRect().width;
+// positioning slides
+const slidesWidth = carouselSlides[0].getBoundingClientRect().width;
+carouselSlides[1].style.left = slidesWidth * 1.5 + 'px';
+carouselSlides[2].style.left = slidesWidth * 2.5 + 'px';
 
-carouselSlides[1].style.left = slideWidth * 1.5 + 'px';
-carouselSlides[2].style.left = slideWidth * 2.5 + 'px';
-
+// gsap timeline
 const carouselTimeline = gsap.timeline({
   repeat: -1,
   ease: 'in-out',
