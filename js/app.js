93e75574c0d5703bf999c62cad96b26f0eb255a4
Carolyn Ndavi <cn.ndavi@hotmail.com>
2022-01-09 13:28:04 +0100

    carousel update

--- js/app.js
+++ js/app.js
@@ -18,76 +18,76 @@ carouselSlides[2].style.left = slidesWidth * 2.5 + 'px';
 // starting variables
 const { gsap, imagesLoaded } = window;
 
-// // gsap timeline
-// const carouselTimeline = gsap.timeline({
-//   repeat: -1,
-//   ease: 'in-out',
-// });
+// gsap timeline
+const carouselTimeline = gsap.timeline({
+  repeat: -1,
+  ease: 'in-out',
+});
 
-// carouselTimeline
-//   .from('.carousel-track-container', {
-//     y: '100%',
-//     opacity: 0,
-//     delay: 1,
-//   })
+carouselTimeline
+  .from('.carousel-track-container', {
+    y: '100%',
+    opacity: 0,
+    delay: 1,
+  })
 
-//   .from('.carousel-slide', {
-//     opacity: 0,
-//     scale: 0.5,
-//     delay: 1,
-//   })
+  .from('.carousel-slide', {
+    opacity: 0,
+    scale: 0.5,
+    delay: 1,
+  })
 
-//   .from('.carousel-navigation', {
-//     opacity: 0,
-//   })
+  .from('.carousel-navigation', {
+    opacity: 0,
+  })
 
-//   .to('li:nth-child(1) ', {
-//     right: '75%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(1) ', {
+    right: '75%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(2)', {
-//     left: '25%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(2)', {
+    left: '25%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(3)', {
-//     left: '75%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(3)', {
+    left: '75%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(1)', {
-//     right: '100%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(1)', {
+    right: '100%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(2)', {
-//     left: '-25%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(2)', {
+    left: '-25%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(3)', {
-//     left: '25%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(3)', {
+    left: '25%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(3)', {
-//     left: '100%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(3)', {
+    left: '100%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(2)', {
-//     left: '75%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(2)', {
+    left: '75%',
+    duration: 1,
+  })
 
-//   .to('li:nth-child(1)', {
-//     right: '25%',
-//     duration: 1,
-//   })
+  .to('li:nth-child(1)', {
+    right: '25%',
+    duration: 1,
+  })
 
-//   .to('.carousel-track-container', {
-//     y: '-100%',
-//     opacity: 0,
-//     delay: 1,
-//   });
+  .to('.carousel-track-container', {
+    y: '-100%',
+    opacity: 0,
+    delay: 1,
+  });
