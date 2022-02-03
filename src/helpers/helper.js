138c93308ecb177e5eda323ea242ca276ff144ec
carolyn <93541667+carolyndavi@users.noreply.github.com>
2022-02-03 21:18:03 +0100

    add helpers

---
+++ src/helpers/helper.js
@@ -0,0 +1,12 @@
+import { days } from './constants'
+
+export const getDays = () => {
+  const today = new Date().getDay()
+  const weeklyDays = []
+
+  for (let i = 0; i < days.length; i++) {
+    weeklyDays.push(days[(today + i) % days.length])
+  }
+
+  return weeklyDays
+}
